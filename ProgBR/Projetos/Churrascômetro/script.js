d33933d47772a49e8e4aba67f4267966bbcc14d4
mmthecoder8 <miguelmoreira01@outlook.com>
2024-02-27 21:04:00 -0300

    I finished churascômetro but I didn't watch the classes to correct yet

--- ProgBR/Projetos/Churrascômetro/script.js
+++ ProgBR/Projetos/Churrascômetro/script.js
@@ -1,23 +1,38 @@
+var a = document.getElementById('a')
+var hmdb = document.getElementById('hmdb')
+var c = document.getElementById('c')
+var d = document.getElementById('d')
+
+var s = document.getElementById('submit')
+
+var op1 = document.getElementById('output1')
+var op2 = document.getElementById('output2')
 
 
 
+function beef(){
+    return((a.value * 500 + c.value * 250) * d.value / 1000)
+
+}
+
+function soda(){
+    return((a.value * 400 + c.value * 300) * d.value / 1000)
+
+}
+
+function beer(){
+    return(hmdb.value * 500 * d.value / 1000)
+}
+
+
+function total(){
+    op1.innerHTML = "You will need " + beef() + "kg of beef"
+    op2.innerHTML = "You will need " + beer() + "L of beer and " + soda() + "L of soda"
+}
 
 
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+s.onclick = total
 
 
 
